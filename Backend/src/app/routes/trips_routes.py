f936160edf14f689d137a28f04d9c367358c5866
CodeByAnkitaM <ankita@accurateic.in>
2025-09-08 10:08:33 +0530

    Added new User page as per new UI design and integrated user table with database

--- Backend/src/app/routes/trips_routes.py
+++ Backend/src/app/routes/trips_routes.py
@@ -1,3 +1,4 @@
+
 from flask import Blueprint
 from src.app.controllers import trips_controller as c
 
@@ -9,3 +10,4 @@ trips_bp.route("/trips/stats/<int:unique_id>", methods=["GET"])(c.trip_stats)
 trips_bp.route("/trips/location/<int:unique_id>", methods=["GET"])(c.trip_location)
 trips_bp.route("/locations/<int:unique_id>", methods=["GET"])(c.raw_locations)
 trips_bp.route("/trips/map/<int:track_id>", methods=["GET"])(c.trip_map)
+trips_bp.route("/users", methods=["GET"])(c.list_users)
